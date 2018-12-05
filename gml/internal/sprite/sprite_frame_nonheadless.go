446a876ab01afd7761daf6526adfc855bb6ff194
Jake B <doogie1012@gmail.com>
2018-12-05 20:23:38 +1100

    example/spaceship: add automatic transparency cropping around images
    Updates #28

--- gml/internal/sprite/sprite_frame_nonheadless.go
+++ gml/internal/sprite/sprite_frame_nonheadless.go
@@ -5,6 +5,7 @@ package sprite
 import (
 	"bytes"
 	"image"
+	"image/draw"
 
 	"github.com/hajimehoshi/ebiten"
 )
@@ -18,11 +19,73 @@ func (frame *SpriteFrame) Size() (width int, height int) { return frame.image.Si
 
 func createFrame(frameData spriteAssetFrame) (SpriteFrame, error) {
 	buf := bytes.NewReader(frameData.Data)
-	image, _, err := image.Decode(buf)
+	imageData, _, err := image.Decode(buf)
 	if err != nil {
 		return SpriteFrame{}, err
 	}
-	sheet, err := ebiten.NewImageFromImage(image, ebiten.FilterDefault)
+
+	// Crop transparency from image
+	{
+		imageSize := imageData.Bounds()
+		top := 0
+	Top:
+		for y := 0; y < imageSize.Size().Y; y++ {
+			for x := 0; x < imageSize.Size().X; x++ {
+				r, g, b, a := imageData.At(x, y).RGBA()
+				//fmt.Printf("r: %v, g: %v, b: %v, a: %v\n", r, g, b, a)
+				if r == 0 && g == 0 && b == 0 && a == 0 {
+					continue
+				}
+				top = y
+				break Top
+			}
+		}
+		left := 0
+	Left:
+		for x := 0; x < imageSize.Size().X; x++ {
+			for y := 0; y < imageSize.Size().Y; y++ {
+				r, g, b, a := imageData.At(x, y).RGBA()
+				//fmt.Printf("r: %v, g: %v, b: %v, a: %v\n", r, g, b, a)
+				if r == 0 && g == 0 && b == 0 && a == 0 {
+					continue
+				}
+				left = x
+				break Left
+			}
+		}
+		right := 0
+	Right:
+		for x := imageSize.Size().Y - 1; x >= 0; x-- {
+			for y := 0; y < imageSize.Size().Y; y++ {
+				r, g, b, a := imageData.At(x, y).RGBA()
+				//fmt.Printf("r: %v, g: %v, b: %v, a: %v\n", r, g, b, a)
+				if r == 0 && g == 0 && b == 0 && a == 0 {
+					continue
+				}
+				right = x
+				break Right
+			}
+		}
+		bottom := 0
+	Bottom:
+		for x := 0; x < imageSize.Size().X; x++ {
+			for y := imageSize.Size().Y - 1; y >= 0; y-- {
+				r, g, b, a := imageData.At(x, y).RGBA()
+				//fmt.Printf("r: %v, g: %v, b: %v, a: %v\n", r, g, b, a)
+				if r == 0 && g == 0 && b == 0 && a == 0 {
+					continue
+				}
+				bottom = y
+				break Bottom
+			}
+		}
+		size := image.Rect(0, 0, right-left, bottom-top)
+		croppedImageData := image.NewRGBA(size)
+		draw.Draw(croppedImageData, size, imageData, image.Point{X: left, Y: top}, draw.Src)
+		//fmt.Printf("left: %v, top: %v, right: %v, bottom: %v\n", left, top, right, bottom)
+	}
+
+	sheet, err := ebiten.NewImageFromImage(imageData, ebiten.FilterDefault)
 	if err != nil {
 		return SpriteFrame{}, err
 	}
