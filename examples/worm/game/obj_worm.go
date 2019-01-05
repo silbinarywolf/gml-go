3106906f948274da57f238910d1ac6dd27848477
Jake B <doogie1012@gmail.com>
2019-01-05 16:30:40 +1100

    example/worm: more work on porting Worm in the Pipes to this engine
    Updates #82

--- examples/worm/game/obj_worm.go
+++ examples/worm/game/obj_worm.go
@@ -1,16 +1,28 @@
 package game
 
-import "github.com/silbinarywolf/gml-go/gml"
+import (
+	"math"
+
+	"github.com/silbinarywolf/gml-go/gml"
+)
 
 type Worm struct {
 	gml.Object
-	//enemyCreateAlarm gml.Alarm
+	Start      gml.Vec
+	SinCounter float64
 }
 
 func (self *Worm) Create() {
 	self.SetSprite(SprWormHead)
 
-	//roomSize := RoomInstanceSize(self.RoomInstanceIndex())
-	self.X = 304
-	self.Y = 528
+	self.Start.X = 304
+	self.Start.Y = 528
+
+	self.Vec = self.Start
+}
+
+func (self *Worm) Update() {
+	self.SinCounter += 0.5
+
+	self.Y = self.Start.Y + math.Round(math.Sin(self.SinCounter*0.15)*21) // y = ystart + round(sin(alarm[1]*0.15)*21); // 0.15, 21
 }
