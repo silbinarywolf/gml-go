db7c8967e0d5accd40aad56b019dabe72ea76109
Jake B <doogie1012@gmail.com>
2019-02-10 16:09:08 +1100

    example/worm: improve test data sample for worm game
    Updates #98

--- examples/worm/test/jump_test.go
+++ examples/worm/test/jump_test.go
@@ -47,34 +47,39 @@ func TestWormJump(t *testing.T) {
 
 			frameInfo := testData[frame]
 			wormInfo := frameInfo[0]
+			bodyParts := frameInfo[1:]
 
 			if inst := game.Global.Player.Get().(*game.Worm); inst != nil {
 				// NOTE(Jake): 2019-02-10
-				// The Game Maker Studio 2 outputs of the float values to 2 decimal places.
-				// So we manipulate our Y position to match 2 decimal places.
-				wormY := math.Floor(inst.Y*100) / 100
+				// Game Maker Studio 2 code seems to be more imprecise with
+				// floating point values than Go. Since the logic is sound
+				// when rounded to 2 decimal places, we simply round the test
+				// data and current simulated game to 2 decimal points for comparison
+				// to ensure the game is loosely running the same way.
+				wormInfoY := math.Round(wormInfo.Y*100) / 100
+				wormY := math.Round(inst.Y*100) / 100
 
 				if wormInfo.X != inst.X ||
-					wormInfo.Y != wormY {
+					wormInfoY != wormY {
 					t.Errorf("Frame %v: Not matching test data\n", frame)
 					if wormInfo.X != inst.X {
 						t.Errorf("- X expected %v but got %v\n", wormInfo.X, inst.X)
 					}
-					if wormInfo.Y != wormY {
-						t.Errorf("- Y expected %v but got %v\n", wormInfo.Y, wormY)
+					if wormInfoY != wormY {
+						t.Errorf("- Y expected %v but got %v\n", wormInfoY, wormY)
 					}
 				}
 
 				for i, _ := range inst.BodyParts {
 					bodyPart := &inst.BodyParts[i]
-					if i+1 >= len(frameInfo) {
+					if i >= len(bodyParts) {
 						if bodyPart.HasSprouted {
 							t.Errorf("- Worm Body Part %d - should not exist.", i)
 						}
 						break
 					}
-					bodyInfo := frameInfo[1+i]
-					bodyPartY := math.Floor(bodyInfo.Y*100) / 100
+					bodyInfo := bodyParts[i]
+					bodyPartY := bodyInfo.Y // math.Floor(bodyInfo.Y*100) / 100
 					if bodyInfo.X != bodyPart.X ||
 						bodyInfo.Y != bodyPartY {
 						t.Errorf("Frame %v: Not matching test data\n", frame)
