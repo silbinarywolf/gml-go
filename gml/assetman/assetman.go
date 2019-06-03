5615c7c5a2568d8ad025d61196212ee8b0949b6e
Jake B <doogie1012@gmail.com>
2019-06-03 18:35:10 +1000

    asset: make asset loading use go routines / be async
    this was done so that WebAssembly builds would download multiple assets at once, as loading is tied to the remote downloading of asset files

--- gml/assetman/assetman.go
+++ gml/assetman/assetman.go
@@ -1,5 +1,9 @@
 package assetman
 
+import (
+	"sync"
+)
+
 var assetManagers []assetManager
 
 var isHasLoadedAll bool
@@ -12,10 +16,17 @@ func LoadAll() {
 	if isHasLoadedAll {
 		panic("Cannot call LoadAll() more than once")
 	}
-	for _, manager := range assetManagers {
-		manager.LoadAll()
+	var wg sync.WaitGroup
+	wg.Add(len(assetManagers))
+	for _, m := range assetManagers {
+		go func(manager assetManager) {
+			manager.LoadAll()
+			wg.Done()
+		}(m)
 	}
+	wg.Wait()
 	isHasLoadedAll = true
+
 }
 
 type assetManager interface {
