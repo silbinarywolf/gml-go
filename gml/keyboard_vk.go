b5aa615ac54b5868010dcdce9bd88e711b1e37b7
Jake B <doogie1012@gmail.com>
2019-12-16 19:59:51 +1100

    gml: add String() method to Key type

--- gml/keyboard_vk.go
+++ gml/keyboard_vk.go
@@ -89,3 +89,96 @@ const (
 	VkZ
 	VkSize
 )
+
+var keyToString = []string{
+	VkNoKey:       "No Key",
+	VkAnykey:      "Any Key",
+	VkLeft:        "Left",
+	VkRight:       "Right",
+	VkUp:          "Up",
+	VkDown:        "Down",
+	VkEnter:       "Enter",
+	VkEscape:      "Escape",
+	VkSpace:       "Space",
+	VkShift:       "Shift",
+	VkControl:     "Control",      // either of the control keys
+	VkAlt:         "Alt",          // alt key
+	VkBackspace:   "Backspace",    // backspace key
+	VkTab:         "Tab",          // tab key
+	VkHome:        "Home",         // home key
+	VkEnd:         "End",          // end key
+	VkDelete:      "Delete",       // delete key
+	VkInsert:      "Insert",       // insert key
+	VkPageUp:      "Page Up",      // pageup key
+	VkPageDown:    "Page Down",    // pagedown key
+	VkPause:       "Pause",        // pause/break key
+	VkPrintScreen: "Print Screen", // printscreen/sysrq key
+	VkF1:          "F1",           // keycode for the function keys F1 to F12
+	VkF2:          "F2",
+	VkF3:          "F3",
+	VkF4:          "F4",
+	VkF5:          "F5",
+	VkF6:          "F6",
+	VkF7:          "F7",
+	VkF8:          "F8",
+	VkF9:          "F9",
+	VkF10:         "F10",
+	VkF11:         "F11",
+	VkF12:         "F12",
+	Vk0:           "0",
+	Vk1:           "1",
+	Vk2:           "2",
+	Vk3:           "3",
+	Vk4:           "4",
+	Vk5:           "5",
+	Vk6:           "6",
+	Vk7:           "7",
+	Vk8:           "8",
+	Vk9:           "9",
+	VkNumpad0:     "Numpad 0",
+	VkNumpad1:     "Numpad 1",
+	VkNumpad2:     "Numpad 2",
+	VkNumpad3:     "Numpad 3",
+	VkNumpad4:     "Numpad 4",
+	VkNumpad5:     "Numpad 5",
+	VkNumpad6:     "Numpad 6",
+	VkNumpad7:     "Numpad 7",
+	VkNumpad8:     "Numpad 8",
+	VkNumpad9:     "Numpad 9",
+	VkNumpadEnter: "Numpad Enter",
+	VkMultiply:    "Numpad Multiply",
+	VkDivide:      "Numpad Divide",
+	VkAdd:         "Numpad Add",
+	VkSubtract:    "Numpad Subtract",
+	VkDecimal:     "Numpad Decimal",
+	VkA:           "A",
+	VkB:           "B",
+	VkC:           "C",
+	VkD:           "D",
+	VkE:           "E",
+	VkF:           "F",
+	VkG:           "G",
+	VkH:           "H",
+	VkI:           "I",
+	VkJ:           "J",
+	VkK:           "K",
+	VkL:           "L",
+	VkM:           "M",
+	VkN:           "N",
+	VkO:           "O",
+	VkP:           "P",
+	VkQ:           "Q",
+	VkR:           "R",
+	VkS:           "S",
+	VkT:           "T",
+	VkU:           "U",
+	VkV:           "V",
+	VkW:           "W",
+	VkX:           "X",
+	VkY:           "Y",
+	VkZ:           "Z",
+}
+
+func (key Key) String() string {
+	return keyToString[key]
+}
